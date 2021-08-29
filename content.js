153c079e815d0e28ef5f20328b42e9521f3ac922
Kirill Borisov <lensvol@gmail.com>
2021-08-29 19:58:50 +0200

    Use more efficient method of tooltip interception.

--- content.js
+++ content.js
@@ -1,4 +1,6 @@
 
+const ITEM_IMAGE_SELECTOR = "img[class='equipped-group__available-item'], img[class='equipped-item__image']";
+
 function createIdAdditionObserver(itemID) {
     return new MutationObserver(function (mutations, observer) {
         for (let m = 0; m < mutations.length; m++) {
@@ -19,9 +21,23 @@ function createIdAdditionObserver(itemID) {
     });
 }
 
-document.addEventListener("mouseover", (event) => {
-    if (event.target && event.target.nodeName === "IMG" && event.target.classList.contains("equipped-group__available-item")) {
-        const itemId = event.target.parentElement.parentElement.attributes["data-quality-id"].value;
-        createIdAdditionObserver(itemId).observe(document, {childList: true, subtree: true});
+let testSlotObserver = new MutationObserver(function (mutations) {
+    for (let m = 0; m < mutations.length; m++) {
+        let mutation = mutations[m];
+
+        for (let n = 0; n < mutation.addedNodes.length; n++) {
+            let node = mutation.addedNodes[n];
+            if (node.nodeName === "DIV") {
+                const itemImages = node.querySelectorAll(ITEM_IMAGE_SELECTOR);
+                for (const img of itemImages) {
+                    img.addEventListener("mouseover", (event) => {
+                        const itemId = event.target.parentElement.parentElement.attributes["data-quality-id"].value;
+                        createIdAdditionObserver(itemId).observe(document, {childList: true, subtree: true});
+                    })
+                }
+            }
+        }
     }
 });
+
+testSlotObserver.observe(document, {childList: true, subtree: true});
