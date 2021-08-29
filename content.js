35d46a725225b36367dcbc422862971149f7a3a7
Kirill Borisov <lensvol@gmail.com>
2021-08-29 20:16:23 +0200

    Make ID display more prominent.

--- content.js
+++ content.js
@@ -4,15 +4,20 @@ const ITEM_IMAGE_SELECTOR = "img[class='equipped-group__available-item'], img[cl
 function createIdAdditionObserver(itemID) {
     return new MutationObserver(function (mutations, observer) {
         for (let m = 0; m < mutations.length; m++) {
-            let mutation = mutations[m];
+            const mutation = mutations[m];
 
             for (let n = 0; n < mutation.addedNodes.length; n++) {
-                let node = mutation.addedNodes[n];
-                if (node.nodeName.toLowerCase() === "div" && node.id.startsWith("tippy-")) {
-                    const idText = `\nID: ${itemID}`;
+                const node = mutation.addedNodes[n];
+                if (node.nodeName === "DIV" && node.id.startsWith("tippy-")) {
+                    const idText = `ID: ${itemID}`;
                     const itemName = node.querySelector("div[class='tooltip__desc'] p span");
-                    if (itemName && !itemName.innerText.endsWith(idText)) {
-                        itemName.innerText += idText;
+                    const existingIdSpan = node.querySelector("strong[class='item-id-display']");
+                    if (itemName && !existingIdSpan) {
+                        const idSpan = document.createElement("strong");
+                        idSpan.innerText = idText;
+                        idSpan.className = 'item-id-display';
+                        idSpan.style.cssText = 'display: block';
+                        itemName.parentElement.insertBefore(idSpan, itemName);
                     }
                     observer.disconnect();
                 }
@@ -23,17 +28,17 @@ function createIdAdditionObserver(itemID) {
 
 let testSlotObserver = new MutationObserver(function (mutations) {
     for (let m = 0; m < mutations.length; m++) {
-        let mutation = mutations[m];
+        const mutation = mutations[m];
 
         for (let n = 0; n < mutation.addedNodes.length; n++) {
-            let node = mutation.addedNodes[n];
-            if (node.nodeName === "DIV") {
+            const node = mutation.addedNodes[n];
+            if (node.nodeName === "DIV" || node.nodeName === "LI") {
                 const itemImages = node.querySelectorAll(ITEM_IMAGE_SELECTOR);
                 for (const img of itemImages) {
                     img.addEventListener("mouseover", (event) => {
                         const itemId = event.target.parentElement.parentElement.attributes["data-quality-id"].value;
                         createIdAdditionObserver(itemId).observe(document, {childList: true, subtree: true});
-                    })
+                    });
                 }
             }
         }
